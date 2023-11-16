9104f9db0963dc844e7ba27ea5db012462943397
sagarmounika <sagarmouni5@gmail.com>
2023-11-16 15:40:47 +0530

    req

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI, Depends, HTTPException,status
-import berserk
+from fastapi import FastAPI
 from app.api.auth import router as auth_router
 from app.api.players import router as chess_router
-from app.security import hash_password, verify_password
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Set this to your front-end's actual domain in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+def read_root():
+    return "Welcome to Chess Dashboard"
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(chess_router, prefix="/chess", tags=["Chess"])
