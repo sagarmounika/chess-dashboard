88eee5bdcbebb78cc773c61f92643a80505d6590
sagarmounika <sagarmouni5@gmail.com>
2023-11-16 18:09:52 +0530

    migrat

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,10 @@
 from logging.config import fileConfig
-
+from app.db.base import Base
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from app.db.base import Base
+from app.models import User,Players,RatingHistory
 from alembic import context
 
-from app.models import User,Players,RatingHistory
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
